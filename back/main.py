f067c8dcb9c2a55d9b0919b832bca4870a9729a2
lennkuz <130076954+lennkuz@users.noreply.github.com>
2024-11-09 16:19:59 +0300

    Update main.py
    add autorization

--- back/main.py
+++ back/main.py
@@ -28,16 +28,38 @@ def post(data=Body()):
     connection = sqlite3.connect('database1.db')
     cursor = connection.cursor()
     try:
-        # Вставка данных в базу
-        cursor.execute('INSERT INTO Users(username, password, TOKEN) VALUES(?, ?, ?);', (username, password, TOKEN))
-        connection.commit()  # Подтверждение изменений
+        cursor.execute('INSERT INTO Users(username, password, TOKEN) VALUES(?, ?, ?);',
+                       (username, password, TOKEN))
+        connection.commit()
     except Exception as e:
         return JSONResponse({'error': str(e)}, status_code=500)
     finally:
-        cursor.close()  # Закрытие курсора
-        connection.close()  # Закрытие соединения
+        cursor.close()
+        connection.close()
 
     return JSONResponse({'TOKEN': TOKEN}, status_code=200)
+
+@app.post('/authorization')
+def post(data=Body()):
+    username = data['username']
+    password = data['password']
+    connection = sqlite3.connect('database1.db')
+    cursor = connection.cursor()
+    cursor.execute(('''SELECT password FROM Users
+    WHERE username = '{}';
+    ''').format(username))
+
+    pas = cursor.fetchall()
+    if pas[0][0] != password:
+
+        return JSONResponse({'error': 'wrong_password'}, status_code=404)
+    else:
+        cursor.execute(('''SELECT TOKEN FROM Users
+                            WHERE username = '{}';
+                            ''').format(username))
+        TOKEN = cursor.fetchone()
+        return JSONResponse({'TOKEN':TOKEN[0]}, status_code=200)
+
 
 
 
